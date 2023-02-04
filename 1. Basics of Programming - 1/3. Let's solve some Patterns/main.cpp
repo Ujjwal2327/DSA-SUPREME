a0afb8ccf0f7690c2bfd97a16f791270bdecd181
Ujjwal2327 <ujjwal2327@gmail.com>
2023-02-05 02:06:17 +0530

    week 1 Basics of Programming - 1 Completed

---
+++ 1. Basics of Programming - 1/3. Let's solve some Patterns/main.cpp
@@ -0,0 +1,288 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // conditions
+        /*// if 
+            int score;
+            cout<<"Enter the score"<<'\n';
+            cin>>score;
+            if(score<300){
+                cout<<"India wins\n";
+            }
+            cout<<"Pakistan wins\n";
+        */
+
+        /*// if-else
+            int score;
+            cout<<"Enter the score"<<'\n';
+            cin>>score;
+            if(score<300){
+                cout<<"India wins\n";
+            }
+            else{
+                cout<<"Pakistan wins\n";
+            }
+        */
+
+        // multiple conditions
+            // int marks;
+            // cin>>marks;
+            
+            /*// nested if-else
+                if(marks>=90){
+                    cout<<"A grade";
+                }
+                else{
+                    if(marks>=80){
+                        cout<<"B grade";
+                    }
+                    else{
+                        if(marks>=60){
+                            cout<<"C grade";
+                        }
+                        else{
+                            if(marks>=40){
+                                cout<<"D grade";
+                            }
+                            else{
+                                cout<<"F grade";
+                            }
+                        }
+                    }
+                }
+            */
+            
+            /*// if-else if-else
+                if(marks>=90){
+                    cout<<"A grade";
+                }
+                else if(marks>=80){
+                    cout<<"B grade";
+                }
+                else if(marks>=60){
+                    cout<<"C grade";
+                }
+                else if(marks>=40){
+                    cout<<"D grade";
+                }
+                else{
+                    cout<<"F grade";
+                }
+            */
+        
+        /*// example
+            int broNum;
+            cin>>broNum;
+            if(broNum==0){
+                cout<<"Baat ban jayegi\n";
+            }
+            else{
+                cout<<"Baat nahi banegi\n";
+            }
+        */
+
+        // loops
+        // for loop
+            /*
+            for(int i=0; i<5; i++){
+                cout << "Love Babbar" <<'\n';
+            }
+            */
+
+            /*
+            for(int i=0; i<5; i++){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=5; i>0; i--){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=1; i<=10; i++){
+                cout << 2*i <<'\n';
+            }
+            */
+            
+            /*
+            for(int i=0; i<=5; i=i+2){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=1; i<=5; i=i+2){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=1; i<10; i=i*2){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=100; i>0; i=i/2){
+                cout << i <<'\n';
+            }
+            */
+
+            /*
+            for(int i=5; (i>=0 && i<=10); i++){
+                cout << i <<'\n';
+            }
+            */
+
+            // what conditions are necessary
+                /*for(int i=0; i<5; i++){
+                    cout << i <<'\n';
+                }
+                */
+
+                /*// removing initialization
+                int i=0;
+                for( ; i<5; i++){
+                    cout << i <<'\n';
+                }
+                */
+
+                /*// removing condition
+                int i=0;
+                for( ; ; i++){
+                    if(i<5){
+                        cout << i <<'\n';
+                    }
+                }
+                */
+                
+                /*// removing updatation
+                // unlimited loop if break is not used
+                int i=0;
+                for( ; ; ){
+                    if(i<5){
+                        cout << i <<'\n';
+                        i++;
+                    }
+                }
+                */
+
+        // patterns
+            /*// pattern 1
+                for(int row=0; row<3; row++){
+                    for(int col=0; col<5; col++){
+                        cout<<'*';
+                    }
+                    cout<<'\n';
+                }
+            */
+            
+            /*// pattern 2
+                int n;
+                cin>>n;
+                for(int row=0; row<n; row++){
+                    for(int col=0; col<n; col++){
+                        cout<<'*';
+                    }
+                    cout<<'\n';
+                }
+            */
+
+            /*// pattern 3
+                int n;
+                cin>>n;
+                for(int row=0; row<3; row++){
+                    for(int col=0; col<5; col++){
+                        if(row==0 || row==n-1){
+                            cout<<"*";
+                        }
+                        else{
+                            if(col==0 || col==4){
+                                cout<<"*";
+                            }
+                            else{
+                                cout<<" ";
+                            }
+                        }
+                    }
+                    cout<<'\n';
+                }
+            */
+            
+            
+            /*// pattern 4
+                int rowCount;
+                int colCount;
+                cin>>rowCount;
+                cin>>colCount;
+                for(int i=0; i<rowCount; i++){
+                    for(int j=0; j<colCount; j++){
+                        if(i==0 || i==rowCount-1){
+                            cout<<"*";
+                        }
+                        else{
+                            if(j==0 || j==colCount-1){
+                                cout<<"*";
+                            }
+                            else{
+                                cout<<" ";
+                            }
+                        }
+                    }
+                    cout<<'\n';
+                }
+            */
+
+            /*// pattern 4    half pyramid
+                int n;
+                cin>>n;
+                for(int row=0; row<n; row++){
+                    for(int col=0; col<row+1; col++){
+                        cout<<'*';
+                    }
+                    cout<<'\n';
+                }
+            */
+            
+            /*// pattern 5    inverted half pyramid
+                int n;
+                cin>>n;
+                for(int row=0; row<n; row++){
+                    for(int col=0; col<n-row; col++){
+                        cout<<'*';
+                    }
+                    cout<<'\n';
+                }
+            */
+
+            /*// pattern 6    half pyramid with numbers
+                int n;
+                cin>>n;
+                for(int i=0; i<n; i++){
+                    for(int j=0; j<i+1; j++){
+                        cout<<j+1;
+                    }
+                    cout<<'\n';
+                }
+            */
+
+            /*// pattern 7
+                int n;
+                cin>>n;
+                for(int i=0; i<n; i++){
+                    for(int j=0; j<n-i; j++){
+                        cout<<j+1;
+                    }
+                    cout<<'\n';
+                }
+            */
+
+            
+
+    return 0;
+}
---
+++ 1. Basics of Programming - 1/4. Week-1 [Connect]/main.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /*// print 1 to 5
+        for(int i=0; i<=5; i++){
+            cout<<i<<' ';
+        }
+    */
+
+    /*// check prime or not
+        int n;
+        cin>>n;
+        bool flag = 1;
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                flag=0;
+            }
+        }
+        if(flag==0){
+            cout<<"Not a prime";
+        }
+        else{
+            cout<<"prime";
+        }
+    */
+
+    // endl can be used as variable name
+    /*// overflow case
+        char endl = 234235;
+        cout<<endl<<'\n';
+    */
+
+    /*// overflow case
+        short s1 = 32767;
+        short s2 = 10;
+        short s = s1+s2;
+        cout<<s;
+    */
+
+    // addition and subtraction
+        /*int a = 5;
+        char b = 'd';
+        int sum = a+b;
+        cout << sum <<'\n';
+        float f= 2.0 + sum;
+        cout << f << '\n';
+        */
+
+        /*float f = 2.7;
+        int n = 157;
+        int diff = n-f;
+        cout<<diff<<'\n';
+        */
+
+        /*int a = 25;
+        float b = 3.5;
+        float diff = a-b;
+        cout << diff;
+        */
+
+    // if else-if else condition without {}
+        /*if(true)
+            cout<<"hello\n";
+        else
+            cout<<"hi\n";
+        */
+    
+    // patterns
+        
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
